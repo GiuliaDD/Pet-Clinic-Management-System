e16b44ebbbc26da616d81af3fb52e9f55735b3d4
GiuliaDD <giulia.diaconescu@gmail.com>
2022-05-19 18:42:03 +0300

    PCMS-14 View all the vaccinated pets

--- src/main/java/com/sda/giulia/petclinic/option/UserOption.java
+++ src/main/java/com/sda/giulia/petclinic/option/UserOption.java
@@ -9,6 +9,7 @@ public enum UserOption {
     DELETE_BY_ID("Delete veterinarian by ID", 3),
     UPDATE("Update veterinarian by ID", 4),
     ADD_NEW_PET("Add new pet", 5),
+    VIEW_ALL_VACCINATED("View all vaccinated pets", 6),
     EXIT("Exit", 999),
     UNKNOWN("Unknown option, try again", 1000);
 
--- src/main/java/com/sda/giulia/petclinic/repository/PetRepository.java
+++ src/main/java/com/sda/giulia/petclinic/repository/PetRepository.java
@@ -3,6 +3,9 @@ package com.sda.giulia.petclinic.repository;
 import com.sda.giulia.petclinic.model.Pet;
 import com.sda.giulia.petclinic.repository.base.BaseRepository;
 
+import java.util.List;
+
 public interface PetRepository extends BaseRepository<Pet, Long> {
 
+    List<Pet> findAllVaccinated();
 }
